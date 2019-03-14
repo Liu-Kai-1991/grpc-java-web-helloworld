074614b9f2c59a3fda856f9ec5ceb9317d11fc2c
Kai Liu <kai_liu@outlook.com>
2019-03-13 23:12:00 -0400

    Add repeated hello and delay hello example

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HelloRequest, HelloReply, RepeatHelloRequest} from '../../generated/main/proto/helloworld_pb'
 import {GreeterClient} from '../../generated/main/proto/helloworld_grpc_web_pb'
 
@@ -13,6 +13,28 @@ request.setName('World');
 client.sayHello(request, {}, (err, response) => {
   console.log(response.getMessage());
 });
+
+
+// server streaming call
+let streamRequest = new RepeatHelloRequest();
+streamRequest.setName('Kai');
+streamRequest.setCount(5);
+
+let stream = client.sayRepeatHello(streamRequest, {});
+stream.on('data', (response) => {
+  console.log(response.getMessage());
+});
+
+
+// deadline exceeded
+let deadline = new Date();
+deadline.setSeconds(deadline.getSeconds() + 1);
+
+client.sayHelloAfterDelay(request, {deadline: deadline.getTime()},
+  (err, response) => {
+    console.log('Got error, code = ' + err.code +
+      ', message = ' + err.message);
+  });
 
 
 @Component({
