0413085c3b78a7c4b9080a17d4af59a3eca24daa
Kai Liu <kai_liu@outlook.com>
2019-03-14 22:13:48 -0400

    Move the response from console to webpage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,40 +1,10 @@
-import {Component} from '@angular/core';
-import {HelloRequest, HelloReply, RepeatHelloRequest} from '../../generated/main/proto/helloworld_pb'
+import {Component, OnInit} from '@angular/core';
+import {
+  HelloRequest,
+  HelloReply,
+  RepeatHelloRequest
+} from '../../generated/main/proto/helloworld_pb'
 import {GreeterClient} from '../../generated/main/proto/helloworld_grpc_web_pb'
-
-
-const client = new GreeterClient('http://' + window.location.hostname + ':8080',
-  null, null);
-
-// simple unary call
-let request = new HelloRequest();
-request.setName('World');
-
-client.sayHello(request, {}, (err, response) => {
-  console.log(response.getMessage());
-});
-
-
-// server streaming call
-let streamRequest = new RepeatHelloRequest();
-streamRequest.setName('Kai');
-streamRequest.setCount(5);
-
-let stream = client.sayRepeatHello(streamRequest, {});
-stream.on('data', (response) => {
-  console.log(response.getMessage());
-});
-
-
-// deadline exceeded
-let deadline = new Date();
-deadline.setSeconds(deadline.getSeconds() + 1);
-
-client.sayHelloAfterDelay(request, {deadline: deadline.getTime()},
-  (err, response) => {
-    console.log('Got error, code = ' + err.code +
-      ', message = ' + err.message);
-  });
 
 
 @Component({
@@ -42,8 +12,51 @@ client.sayHelloAfterDelay(request, {deadline: deadline.getTime()},
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  client = new GreeterClient('http://' + window.location.hostname + ':8080',
+    null, null);
+  name: string;
+  timeOut: number;
+  serverResponse: string = "";
+
+  constructor() {
+  }
+
+  sendRequest() {
+    let request = new HelloRequest();
+    request.setName(this.name);
+    this.client.sayHello(request, {}, (err, response) => {
+      this.serverResponse = this.serverResponse + response.getMessage() + "\n";
+    });
+    // server streaming call
+    let streamRequest = new RepeatHelloRequest();
+    streamRequest.setName('Kai');
+    streamRequest.setCount(5);
+
+    let stream = this.client.sayRepeatHello(streamRequest, {});
+    stream.on('data', (response) => {
+      this.serverResponse = this.serverResponse + response.getMessage() + "\n";
+    });
+
+    // deadline exceeded
+    let deadline = new Date();
+    deadline.setSeconds(deadline.getSeconds() + this.timeOut);
+
+    this.client.sayHelloAfterDelay(request, {deadline: deadline.getTime()},
+      (err, response) => {
+        if (err != null) {
+          this.serverResponse = this.serverResponse + 'Got error, code = ' + err.code +
+            ', message = ' + err.message;
+        }
+        if (response != null) {
+          this.serverResponse = this.serverResponse + response.getMessage() + "\n";
+        }
+      });
+  }
+
+  ngOnInit() {
+  }
 }
 
 
